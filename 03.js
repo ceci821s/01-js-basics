c229b4644dd266f49b862b6619416cd4c624c838
Cecilie Dinsen <ceci821s@stud.kea.dk>
2022-01-31 12:27:10 +0100

    knap og tekst

---
+++ 03.js
@@ -0,0 +1,5 @@
+document.querySelector("button").addEventListener("click", buttonClicked);
+function buttonClicked() {
+  let brugerNavn = document.querySelector("#navn").value;
+  console.log(`Tillykke dit navn er ${brugerNavn}`);
+}
